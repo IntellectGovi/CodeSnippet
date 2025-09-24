11aa6dc7959e50a07fc1a961e2a0fb945f86cac1
Govind <govind.upadhayay19@gmail.com>
2025-09-24 09:33:22 +0530

    added

--- frontend/src/Pages/Contact.jsx
+++ frontend/src/Pages/Contact.jsx
@@ -1,6 +1,4 @@
 import React from "react"
-
-import ContactDetails from "../components/core/AboutUs/ContactDetails"
 import ContactForm from "../components/core/AboutUs/ContactForm"
 
 
@@ -9,13 +7,10 @@ const Contact = () => {
   return (
     <div>
       <div className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row">
-        {/* Contact Details */}
-        <div className="lg:w-[40%]">
-          <ContactDetails />
-        </div>
+        
 
         {/* Contact Form */}
-        <div className="lg:w-[60%]">
+        <div className="mb-20 w-full">
           <ContactForm />
         </div>
       </div>
