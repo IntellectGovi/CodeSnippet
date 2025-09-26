bbcb39f2be97630481e27c59ea2fd03573d75235
Govind <govind.upadhayay19@gmail.com>
2025-09-26 08:36:02 +0530

    addedd

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,21 +5,28 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import setupLocatorUI from "@locator/runtime";
 import Preloader from "./components/PreLoader/PreLoader.jsx";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 if (process.env.NODE_ENV === "development") {
   setupLocatorUI();
 }
 
+
+
 function Root() {
-  
+  const [loading, setLoading] = useState(true);
+  const [isFirstLoad , setIsFirstLoad] = useState(false);
 
   return (
     <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
+      <Provider store={store}>
+        <StrictMode>
+          {loading && <Preloader onComplete={() => setLoading(false)} />}
+          <App />
+        </StrictMode>
+      </Provider>
     </BrowserRouter>
   );
 }
 
 createRoot(document.getElementById("root")).render(<Root />);
-
